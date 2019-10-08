d15093cd8f261a754a69288d120d8643e6e59e5c
Fernanda Garcia <fg14012013@gmail.com>
2019-10-08 11:56:13 -0600

    Practica 9

---
+++ Practica9/Ejercicios/Ejercicio1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica9
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            List<string> frutas = new List<string>();
+            frutas.Add("1.Manzana");
+            frutas.Add("2.Mango");
+            frutas.Add("3.Jocote");
+            frutas.Add("4.Piña");
+            frutas.Add("5.Fresa");
+
+            int op;
+            Console.WriteLine("Menu principal");
+            Console.WriteLine("[1].Agregar a la lista");
+            Console.WriteLine("[2].Mostrar lista");
+            Console.WriteLine("[3].Insertar en la lista");
+            Console.WriteLine("[4].Eliminar de la lista");
+            Console.WriteLine("[5].Buscar en la lista");
+            Console.WriteLine("[6].Vaciar lista");
+            Console.WriteLine("[7].Salir");
+            Console.WriteLine("--------------------------");
+            op = Convert.ToInt32(Console.ReadLine());
+
+            switch (op)
+            {
+                //Agregar fruta
+                case 1:
+                    string fruta;
+                    Console.Clear();
+                    Console.WriteLine("Cantidad de frutas que desea agregar");
+                    int cant = Convert.ToInt32(Console.ReadLine());
+                    int[] agregar = new int[cant];
+                    for (int i = 0; i < agregar.Length; i++)
+                    {
+
+                        Console.Write("Fruta: ");
+                        fruta = Console.ReadLine();
+
+
+                        frutas.Add(fruta);
+                    }
+
+                    Console.Clear();
+                    foreach (string fr in frutas)
+                    {
+                        Console.WriteLine(fr);
+                    }
+                    break;
+
+
+                //Mostrar lista
+                case 2:
+                    Console.Clear();
+                    foreach (string fr in frutas)
+                    {
+                        Console.WriteLine(fr);
+                    }
+                    break;
+
+                //Insertar en la lista
+                case 3:
+                    Console.Clear();
+                    int num1;
+                    string nueva;
+                    Console.WriteLine("Ingrese el numero del indice donde desea agregar la fruta");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingrese la nueva fruta agregar");
+                    nueva = Console.ReadLine();
+                    frutas.Insert(num1, nueva);
+
+                    Console.Clear();
+                    foreach (string fr in frutas)
+                    {
+                        Console.WriteLine(fr);
+                    }
+
+                    break;
+
+                //Eliminar de la lista
+                case 4:
+                    Console.Clear();
+                    int num;
+                    Console.WriteLine("Ingrese el numero de la fruta que desea eliminar");
+                    num = Convert.ToInt32(Console.ReadLine());
+                    frutas.RemoveAt(num);
+
+                    Console.Clear();
+                    foreach (string fr in frutas)
+                    {
+                        Console.WriteLine(fr);
+                    }
+                    break;
+
+                //Buscar en la lista
+                case 5:
+                    Console.Clear();
+                    string b;
+                    Console.WriteLine("Ingrese la fruta que desea buscar");
+                    b = Console.ReadLine();
+
+                    do
+                    {
+
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.WriteLine("La fruta se encuentra en la lista.");
+
+
+                    } while ((frutas.Contains(b)));
+
+
+                    break;
+
+                //Vaciar lista
+                case 6:
+                    Console.Clear();
+                    frutas.Clear();
+                    break;
+
+                //Salir
+                case 7:
+                    Console.Clear();
+                    Console.WriteLine("Presione <Enter> para salir.");
+                    break;
+
+
+
+
+
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Practica9/Ejercicios/Ejercicio2.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica9.Ejercicios
+{
+    class Ejercicio2
+    {
+        public struct Alumnos
+        {
+            public string carnet;
+            public string nombre;
+            public string carrera;
+            private double cum;
+
+
+            public void setCum(double cum)
+            {
+                if (cum >= 0)
+                {
+                    if (cum <= 10)
+                    {
+                        this.cum = cum;
+                    }
+
+
+                }
+            }
+
+            public double getCum()
+            {
+                return cum;
+            }
+
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, Alumnos> datosA = new Dictionary<string, Alumnos>();
+
+
+            int opc;
+            Console.WriteLine("  Menu Principal.");
+            Console.WriteLine("  [1].Agregar Alumno ");
+            Console.WriteLine("  [2].Mostrar Alumnos ");
+            Console.WriteLine("  [3].Eliminar Alumno ");
+            Console.WriteLine("  [4].Buscar Alumno ");
+            Console.WriteLine("  [6].Vaciar Diccionario ");
+            Console.WriteLine("  [7].Salir ");
+            Console.WriteLine("-------------------------");
+            opc = Convert.ToInt32(Console.ReadLine());
+            int cant;
+
+            switch (opc)
+            {
+                //Agregar alumno
+                case 1:
+                    Console.Clear();
+                    Console.Write("Cuantos alumnos desea ingresar: ");
+                    cant = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    for (int i = 0; i < cant; i++)
+                    {
+                        Alumnos A = new Alumnos();
+                        Console.WriteLine("Alumno #{0}", i + 1);
+                        do
+                        {
+                            Console.Write("Ingrese el carnet del alumno: ");
+                            A.carnet = Console.ReadLine();
+                            if (datosA.ContainsKey(A.carnet))
+                            {
+                                Console.WriteLine("El carnet: {0} ya existe...", A.carnet);
+                            }
+
+                        } while (datosA.ContainsKey(A.carnet));
+
+
+                        Console.Write("Ingrese el nombre del Alumno: ");
+                        A.nombre = Console.ReadLine();
+                        Console.Write("Ingrese la carrera del Alumno: ");
+                        A.carrera = Console.ReadLine();
+                        Console.WriteLine("Ingrese el CUM del Alumno: ");
+                        A.setCum(Convert.ToDouble(Console.ReadLine()));
+                        datosA.Add(A.carnet, A);
+
+
+
+
+
+                    }
+
+
+
+                    break;
+
+                //Mostrar alumnos
+                case 2:
+                    Console.Clear();
+                    foreach (KeyValuePair<string, Alumnos> datos in datosA)
+                    {
+                        Console.WriteLine(datos);
+                    }
+                    break;
+
+                //Eliminar alumno
+                case 3:
+                    Console.Clear();
+                    string e;
+                    Console.WriteLine("Ingrese el carnet del alumno que desea eliminar");
+                    e = Console.ReadLine();
+                    datosA.Remove(e);
+                    foreach (KeyValuePair<string, Alumnos> datos in datosA)
+                    {
+                        Console.WriteLine(datos);
+                    }
+
+                    break;
+
+                //Buscar alumno
+                case 4:
+                    Console.Clear();
+                    string b;
+                    Console.WriteLine("Ingrese el alumno que desea buscar");
+                    b = Console.ReadLine();
+
+                    do
+                    {
+
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.WriteLine("El alumno se encuentra en la lista.");
+
+
+                    } while ((datosA.ContainsKey(b)));
+                    break;
+
+                //Vaciar diccionario
+                case 5:
+                    Console.Clear();
+                    datosA.Clear();
+                    break;
+
+                //Salir
+                case 6:
+                    Console.Clear();
+                    Console.WriteLine("Presione <Enter> para salir");
+                    break;
+
+
+
+            }
+
+
+
+
+
+        }
+
+
+    }
+}
